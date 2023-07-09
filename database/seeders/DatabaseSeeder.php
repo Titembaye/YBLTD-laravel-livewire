47f919e295698efdc7cc20043a52765161359963
Titembaye <donaldtitembaye@gmail.com>
2023-07-09 22:08:26 +0000

    les migrations, facories et seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Client;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(TypeArticleSeeder::class);
+        Article::factory(50)->create();
+        Client::factory(50)->create();
+        $this->call(DureeLocationSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(StatutLocationSeeder::class);
+        $this->call(RoleSeeder::class);
     }
 }
